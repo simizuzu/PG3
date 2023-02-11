8b06a430f383aeb48424a76a3b3e0810dc6f5486
翔 清水 <k021g1379@g.neec.ac.jp>
2023-02-12 01:29:59 +0900

    前のものを削除

--- PG31-3/IShape.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class IShape
-{
-public:
-	virtual ~IShape() = default;
-	virtual void Draw() = 0;
-	virtual float GetSize() = 0;
-};
-
--- PG31-3/main.cpp
+++ PG31-3/main.cpp
@@ -3,30 +3,8 @@
 #include<memory>
 #include<vector>
 
-#include"IShape.h"
-#include"Rectangle.h"
-#include"Circle.h"
-
 int main()
 {
-	std::vector<std::unique_ptr<IShape>>shapes;
-
-	shapes.push_back(std::move(std::make_unique<Circle>(5.0f)));
-	shapes.push_back(std::move(std::make_unique<Rectangle>(10.f, 20.f)));
-
-
-	for (std::unique_ptr<IShape>& shape : shapes)
-	{
-		printf("取得した値は%f\n", shape->GetSize());
-	}
-	printf("\n");
-
-	// 描画
-	for (std::unique_ptr<IShape>& shape : shapes)
-	{
-		shape->Draw();
-	}
-
 
 	system("pause");
 
